2707bb6ea0fc255bc8f4580a6f06244af18388fd
Miguel <44586245+MiguelFMD@users.noreply.github.com>
2025-11-20 15:48:37 +0100

    Terminada practica UI

--- Assets/Scripts/Tarea2/RotacionYMovimientoLocal2D.cs
+++ Assets/Scripts/Tarea2/RotacionYMovimientoLocal2D.cs
@@ -3,24 +3,42 @@ using UnityEngine.InputSystem;
 
 public class RotacionYMovimientoLocal2D : MonoBehaviour
 {
-    // Velocidad de traslación hacia adelante (unidades por segundo)
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float velocidadNormal = 5f;
+    [SerializeField] private float velocidadTurbo = 10f;
+
     // Velocidad de rotación (grados por segundo)
     [SerializeField] private float rotationSpeed = 180f;
 
     private Rigidbody2D rb2d;
-    // Entradas leídas en Update y aplicadas en FixedUpdate
+
+    private float velocidadActual;
+
     private float horizontalInput;
     private float verticalInput;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+
+        velocidadActual = velocidadNormal;
+    }
+
+    public void CambiarModoTurbo(bool activo)
+    {
+        if (activo)
+        {
+            velocidadActual = velocidadTurbo;
+            Debug.Log("Modo Turbo ACTIVADO: " + velocidadActual);
+        }
+        else
+        {
+            velocidadActual = velocidadNormal;
+            Debug.Log("Modo Turbo DESACTIVADO: " + velocidadActual);
+        }
     }
 
     void Update()
     {
-
         float left = (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) ? -1f : 0f;
         float right = (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed) ? 1f : 0f;
         horizontalInput = left + right;
@@ -32,19 +50,19 @@ public class RotacionYMovimientoLocal2D : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Usar Time.fixedDeltaTime en física
-        float rotationDelta = horizontalInput * rotationSpeed * Time.fixedDeltaTime;
-        float forwardDistance = verticalInput * speed * Time.fixedDeltaTime;
-
         if (rb2d != null)
         {
-            // Rotación física: MoveRotation espera un ángulo absoluto (grados)
+            float rotationDelta = horizontalInput * rotationSpeed * Time.fixedDeltaTime;
+
+            
+            float forwardDistance = verticalInput * velocidadActual * Time.fixedDeltaTime;
+
+            // Rotación física
             float newRotation = rb2d.rotation - rotationDelta;
             rb2d.MoveRotation(newRotation);
 
-            // Avance en dirección "adelante" según la nueva rotación
-            float rad = newRotation * Mathf.Deg2Rad;
             Vector2 forward = (Vector2)(Quaternion.Euler(0f, 0f, newRotation) * Vector2.up);
+
             Vector2 newPosition = rb2d.position + forward * forwardDistance;
             rb2d.MovePosition(newPosition);
         }
--- README.md
+++ README.md
@@ -1,8 +1,26 @@
 # UI-Eventos
 
-## Activar proyectil
+## Ej 1: Activar proyectil
 
 ![](docs/images/activate_sprite.gif)
 
-## Animacion proyectil
+## Ej 2 y 3
+
+![](docs/images/proyectil_en_padre.gif)
+
+## Ej 4 : Ocultamiento automático
+
+![](docs/images/corutina_proyectil.gif)
+
+## Ej 6: Prefab e instanciación
+
+![](docs/images/disparo_desde_jugador.gif)
+
+## Ej 7: Eventos
+
+![](docs/images/activar_por_colision_ej7.gif)
+
+## Ej 8 : Modo turbo
+
+![](docs/images/modo_turbo.gif)
 
