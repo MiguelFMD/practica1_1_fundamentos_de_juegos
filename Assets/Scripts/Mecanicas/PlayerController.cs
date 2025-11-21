4ae42d3326b162fb0634961f794c76a21d8bef10
Miguel <44586245+MiguelFMD@users.noreply.github.com>
2025-11-21 10:23:46 +0100

    Salto

---
+++ Assets/Scripts/Mecanicas/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float fuerzaSalto = 5f;
+    private Rigidbody2D rb2D;
+    private bool isJumping = false;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame && !isJumping)
+        {
+            rb2D.AddForce(transform.up * fuerzaSalto, ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Suelo"))
+        {
+            isJumping = false;
+            rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, 0);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,26 +1,5 @@
-# UI-Eventos
+# Mecanicas
 
-## Ej 1: Activar proyectil
+## Salto basico
 
-![](docs/images/activate_sprite.gif)
-
-## Ej 2 y 3
-
-![](docs/images/proyectil_en_padre.gif)
-
-## Ej 4 : Ocultamiento automático
-
-![](docs/images/corutina_proyectil.gif)
-
-## Ej 6: Prefab e instanciación
-
-![](docs/images/disparo_desde_jugador.gif)
-
-## Ej 7: Eventos
-
-![](docs/images/activar_por_colision_ej7.gif)
-
-## Ej 8 : Modo turbo
-
-![](docs/images/modo_turbo.gif)
-
+![](docs/images/salto_tocando_suelo.gif)
